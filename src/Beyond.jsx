224af0fb99c2c25f581bdded8ef13a45dd220d2a
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-10-29 23:29:35 -0400

    fixing images

--- src/Beyond.jsx
+++ src/Beyond.jsx
@@ -10,11 +10,11 @@ const Beyond = () => {
         <h3>National Japan Bowl</h3>
         <p> Every year, students from Blair compete at the <a href="https://japanbowl.org/31st-national-japan-bowl/">National Japan Bowl</a>. Nostrud cillum id laboris minim proident minim Lorem est laboris labore deserunt duis.</p>
         <figure>
-          <img src="public/Japan Bowl 4-13-23.jpeg" alt="class at Japan Bowl 2023" />
+          <img src="/public/Japan Bowl 4-13-23.jpeg" alt="class at Japan Bowl 2023" />
           <figcaption>Three students with Instructor Yoko Zoll at Japan Bowl 2023</figcaption>
         </figure>
         <figure>
-          <img src="public/Japan Bowl 4-13-23 2 2.jpeg" alt="class at Japan Bowl 2023" />
+          <img src="/public/Japan Bowl 4-13-23 2 2.jpeg" alt="class at Japan Bowl 2023" />
           <figcaption>At a banquet table during Japan Bowl 2023.</figcaption>
         </figure>
       </div>
@@ -22,15 +22,15 @@ const Beyond = () => {
         <h3>Visit to the White House</h3>
         <p> In 2015 students met former Japanese Prime Minister Shinzo Abe at the White House.  Nostrud cillum id laboris minim proident minim Lorem est laboris labore deserunt duis.</p>
         <figure>
-          <img src="public/whitehousevisit-1.jpeg" alt="class at whitehouse" />
+          <img src="/public/whitehousevisit-1.jpeg" alt="class at whitehouse" />
           <figcaption>Blair students at the White House South Lawn.</figcaption>
         </figure>
         <figure>
-          <img src="public/whitehousevisit-2.jpeg" alt="class at whitehouse" />
+          <img src="/public/whitehousevisit-2.jpeg" alt="class at whitehouse" />
           <figcaption>Blair students amongst the crowd.</figcaption>
         </figure>
         <figure>
-          <img src="public/whitehousevisit-3.jpeg" alt="class at whitehouse" />
+          <img src="/public/whitehousevisit-3.jpeg" alt="class at whitehouse" />
           <figcaption>Podium where Former President Obama and Former Prime Minister Abe addressed the crowd.</figcaption>
         </figure>
       </div>

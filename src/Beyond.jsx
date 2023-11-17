fb5b77dfd4117c6d0d3bc6559eae4845cdaa88c3
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-11-17 09:35:23 -0500

    added more event photos

--- src/Beyond.jsx
+++ src/Beyond.jsx
@@ -14,14 +14,22 @@ import japantrip6 from "/japantrip-06.jpg";
 import japantrip7 from "/japantrip-07.jpg";
 import japantrip8 from "/japantrip-08.jpg";
 import japantrip9 from "/japantrip-09.jpg";
+import japantrip10 from "/jlp-class2017.jpg"
+import japantrip11 from "/japantrip11.jpg"
+import japantrip12 from "/japantrip12.jpg"
 import onigiri1 from "/IMG_3979.jpg";
 import onigiri2 from "/IMG_3980.jpg";
-import onigiri3 from "/IMG_3981.jpg";
 import onigiri4 from "/IMG_3982.jpg";
 import onigiri5 from "/IMG_3983.jpg";
-import onigiri6 from "/IMG_3984.jpg";
 import onigiri7 from "/IMG_3985.jpg";
 import onigiri8 from "/IMG_3986.jpg";
+import akimatsuri1 from "/akimatsuri01.jpg";
+import akimatsuri2 from "/akimatsuri02.jpg";
+import akimatsuri3 from "/akimatsuri03.jpg";
+import akimatsuri5 from "/akimatsuri05.jpg";
+import akimatsuri6 from "/akimatsuri06.jpg";
+import akimatsuri7 from "/akimatsuri07.jpg";
+
 
 const Beyond = () => {
   return (
@@ -32,7 +40,7 @@ const Beyond = () => {
 
       <div className="topic">
         <h3>Onigiri Action 2023</h3>
-        <p>Blair's JLP participates in Onigiri Action, a six week event that happens every fall to raise money for hunger. The initiative began in Japan 16 years ago and has since spread world-wide. Learn more about <a href="https://usa.tablefor2.org/onigiri-action">#Onigiri Action</a> organized by <a href="https://usa.tablefor2.org/">Table for Two USA</a>. Each year, Blair's JLP classes make up to 15 pounds of rice to raise money for hunger. The event was also featured in <a href="https://silverchips.mbhs.edu/content/blair-hosts-onigiri-making-event-in-media-center-36103/">Silver Chips</a>.</p>
+        <p>Blair's JLP participates in Onigiri Action, a six week event that happens every fall to raise money for hunger. The initiative began in Japan 16 years ago and has since spread world-wide. Learn more about <a href="https://usa.tablefor2.org/onigiri-action">#Onigiri Action</a> organized by <a href="https://usa.tablefor2.org/">Table for Two USA</a>. Each year, Blair's JLP classes make up to 15 pounds of rice to raise money for hunger. The event was also featured in Blair's school newsletter <a href="https://silverchips.mbhs.edu/content/blair-hosts-onigiri-making-event-in-media-center-36103/">Silver Chips Online</a>.</p>
 
         <figure className="bigger">
           <img src={onigiri1} alt="students during onigiri action 2023" />
@@ -41,16 +49,10 @@ const Beyond = () => {
           <img src={onigiri2} alt="students during onigiri action 2023" />
         </figure>
         <figure className="bigger">
-          <img src={onigiri3} alt="students during onigiri action 2023" />
-        </figure>
-        <figure className="bigger">
           <img src={onigiri4} alt="students during onigiri action 2023" />
         </figure>
         <figure className="bigger">
           <img src={onigiri5} alt="students during onigiri action 2023" />
-        </figure>
-        <figure className="bigger">
-          <img src={onigiri6} alt="students during onigiri action 2023" />
         </figure>
         <figure className="bigger">
           <img src={onigiri7} alt="students during onigiri action 2023" />
@@ -58,10 +60,7 @@ const Beyond = () => {
         <figure className="bigger">
           <img src={onigiri8} alt="students during onigiri action 2023" />
         </figure>
-
-
       </div>
-
 
       <div className="topic">
         <h3>National Japan Bowl</h3>
@@ -75,9 +74,37 @@ const Beyond = () => {
           <figcaption>At a banquet table during Japan Bowl 2023.</figcaption>
         </figure>
       </div>
+
+      <div className="topic">
+        <h3>Akimatsuri Fall Festival</h3>
+        <p>On November 4 2017, Blair students participated in the Japanese Fall Festival at the National Arboretum in Washington, DC. </p>
+
+        <figure className="bigger">
+          <img src={akimatsuri2} alt="students at a fall festival" />
+          <figcaption>Blair students with Instructor Yoko Zoll.</figcaption>
+        </figure>
+        <figure className="bigger">
+          <img src={akimatsuri3} alt="students at a fall festival" />
+          <figcaption>Blair students at Fall Festival.</figcaption>
+        </figure>
+        <figure className="bigger">
+          <img src={akimatsuri1} alt="students at a fall festival" />
+        </figure>
+        <figure className="bigger">
+          <img src={akimatsuri5} alt="students at a fall festival" />
+        </figure>
+        <figure className="bigger">
+          <img src={akimatsuri6} alt="students at a fall festival" />
+        </figure>
+        <figure className="bigger">
+          <img src={akimatsuri7} alt="students at a fall festival" />
+          <figcaption>Instructor Zoll between women dressed in traditional Kimonos.</figcaption>
+        </figure>
+      </div>
+
       <div className="topic">
         <h3>Visit to the White House</h3>
-        <p> In 2015 students met former Japanese Prime Minister Shinzo Abe at the White House.</p>
+        <p> In 2015 students met former Japanese Prime Minister Shinzo Abe and former President Barack Obama at the White House.</p>
         <figure className="bigger">
           <img src={whitehouse1} alt="class at whitehouse" />
           <figcaption>Blair students at the White House South Lawn.</figcaption>
@@ -95,25 +122,44 @@ const Beyond = () => {
           <figcaption>Blair students outside of the Japanese Embassy.</figcaption>
         </figure>
       </div>
+
+      <div className="topic">
+        <h3>Local Performances</h3>
+        <p>In 2015, a Japanese dancer Shizumi performed at Blair's auditorium.</p>
+        <figure className="bigger">
+          <img src={japantrip3} alt="class in Japan" />
+          <figcaption>On stage with Blair students.</figcaption>
+        </figure>
+        <figure className="bigger">
+          <img src={japantrip6} alt="class in Japan" />
+          <figcaption>On stage with Blair students.</figcaption>
+        </figure>
+      </div>
+
       <div className="topic">
         <h3>Trips to Japan</h3>
         <p> In 2010, 2014 and 2019 Blair students visited Japan. </p>
+        <figure className="bigger">
+          <img src={japantrip10} alt="class in Japan" />
+          <figcaption>Kakehashi Japan Bowl 2019 group picture.</figcaption>
+        </figure>
+        <figure className="bigger">
+          <img src={japantrip11} alt="class in Japan" />
+          <figcaption>Blair students with Mrs. Akie Abe.</figcaption>
+        </figure>
+        <figure className="bigger">
+          <img src={japantrip12} alt="class in Japan" />
+          <figcaption>Blair students with Mrs. Akie Abe.</figcaption>
+        </figure>
         <figure className="bigger">
           <img src={japantrip1} alt="class in Japan" />
           <figcaption>Traditional Japanese meal.</figcaption>
         </figure>
         <figure className="bigger">
           <img src={japantrip2} alt="class in Japan" />
-          <figcaption>At a Shinto Buddhist Temple.</figcaption>
+          <figcaption>Buddah in Kamakura.</figcaption>
         </figure>
-        <figure className="bigger">
-          <img src={japantrip3} alt="class in Japan" />
-          <figcaption>At a performance on stage.</figcaption>
-        </figure>
-        <figure className="bigger">
-          <img src={japantrip6} alt="class in Japan" />
-          <figcaption>At a performance on stage.</figcaption>
-        </figure>
+       
         <figure className="bigger">
           <img src={japantrip4} alt="class in Japan" />
           <figcaption>Visiting a Japanese high school.</figcaption>
@@ -128,13 +174,12 @@ const Beyond = () => {
         </figure>
         <figure className="bigger">
           <img src={japantrip8} alt="class in Japan" />
-          <figcaption>Blair students in Japan.</figcaption>
+          <figcaption>Blair students at the entrance of Ghibli Museum in Tokyo.</figcaption>
         </figure>
         <figure className="bigger">
           <img src={japantrip9} alt="class in Japan" />
           <figcaption>Blair students in matching kimonos.</figcaption>
         </figure>
-
       </div>
     </div>
   )
